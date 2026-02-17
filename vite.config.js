075d9d8d782d7cb6dfce34094057395fca5df5b7
ZydZaidan <m.yazidzaidan13@gmail.com>
2026-02-18 02:34:00 +0700

    install pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa' // Import ini
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Smart Presence PT BEST',
+        short_name: 'PresenceBEST',
+        description: 'Aplikasi Absensi Karyawan PT BEST',
+        theme_color: '#2563eb', // Warna Biru Dashboard kamu
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
